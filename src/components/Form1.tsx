6028093cbc5341cb28e34814e0248577b6f0f34d
Shershneva Kseniya <megacoolksuha@mail.ru>
2024-08-16 23:35:57 +0300

    feat: add components "Forms", "Form1", "Form2"

--- src/components/Form1.tsx
+++ src/components/Form1.tsx
@@ -1,5 +1,94 @@
+import { useRef, FormEvent } from 'react';
+
 function Form1() {
-  return <div>form 111111</div>;
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    if (inputRef.current) {
+      alert(`Name: ${inputRef.current.value}`);
+    }
+  }
+
+  return (
+    <div className="modal_window ">
+      <div className="wrapper_form">
+        <div className="form_header">
+          <div>
+            <h1>FORM 1</h1>
+            <p>Create a form:</p>
+          </div>
+
+          <div className="close_modal"></div>
+        </div>
+
+        <div className="form_body">
+          <form onSubmit={handleSubmit}>
+            <label>
+              Name:
+              <input type="text" ref={inputRef}></input>
+            </label>
+
+            <label>
+              Age:
+              <input type="number"></input>
+            </label>
+
+            <label>
+              Email:
+              <input type="email" />
+            </label>
+
+            <label>
+              Password:
+              <input type="password" />
+            </label>
+
+            <label>
+              Confirm password:
+              <input type="password" />
+            </label>
+
+            <label>
+              Gender:
+              <div>
+                <label className="radio_button">
+                  <input
+                    type="radio"
+                    name="gender"
+                    value="male"
+                    // checked={gender === 'male'}
+                    // onChange={e => setGender(e.target.value)}
+                  />
+                  Male
+                </label>
+
+                <label className="radio_button">
+                  <input
+                    type="radio"
+                    name="gender"
+                    value="female"
+                    // checked={gender === 'female'}
+                    // onChange={e => setGender(e.target.value)}
+                  />
+                  Female
+                </label>
+              </div>
+            </label>
+
+            <label>
+              Message:
+              <textarea />
+            </label>
+
+            <button className="button_submit" type="submit">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Form1;
--- src/components/Forms.tsx
+++ src/components/Forms.tsx
@@ -1,10 +1,28 @@
+import { Outlet, useNavigate } from 'react-router-dom';
+
 function Forms() {
+  const navigate = useNavigate();
+
+  const handleClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    navigate({ pathname: `/form1` });
+  };
+
   return (
-    <>
-      <div>Главная страница</div>
-      <button>form 1</button>
-      <button>form 2</button>
-    </>
+    <main>
+      <header>
+        <h1>FORMS</h1>
+        <div className="header_content">
+          <button onClick={handleClick}>form 1</button>
+          <button>form 2</button>
+          <Outlet />
+        </div>
+      </header>
+      <section>
+        <div className="left_section_search_result"> sdfsdfsdfsdf</div>
+        <div className="right_section_search_result"> sdfs fghfgh fghfgh</div>
+      </section>
+    </main>
   );
 }
 
